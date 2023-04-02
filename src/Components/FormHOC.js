13db3d492f052bfc27c0136dd6ced3153e912c07
princebhatia09 <princebhatia09@gmail.com>
2023-04-02 17:48:03 +0530

    uild

--- src/Components/FormHOC.js
+++ src/Components/FormHOC.js
@@ -3,18 +3,13 @@ import React,{ useState } from "react"
 const withUseFormHoc = (Component)=>{
 
     return props => {
-        // const { register, handleSubmit, errors,control } = useForm()
-        const [message, setMessage] = useState();
+        const [message] = useState();
         const onSubmit = (event,editData) => {
             event.preventDefault()
 
 
         }
         return <Component
-            // control={control}
-            // register={register}
-            // handleSubmit={handleSubmit}
-            // errors={errors}
             onSubmit={onSubmit}
             message = {message}
             {...props} />
